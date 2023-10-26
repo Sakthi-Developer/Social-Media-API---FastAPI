1a62156ce35ae89d0a0d0d85b9260d692ae6dce8
Sakthi <sakthir.developer@gmail.com>
2023-10-26 19:33:25 +0530

    commit

--- app/models.py
+++ app/models.py
@@ -19,3 +19,8 @@ class User(Base):
     email = Column(String,unique=True,nullable=False)
     password = Column(String,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+
+class Votes(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE"), primary_key= True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key= True)
