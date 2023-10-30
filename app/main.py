992c52ea2a3a20696c96109b40c307f9878b2cd2
Sakthi <sakthir.developer@gmail.com>
2023-10-30 22:25:58 +0530

    its commited with a error with id(crnt usr)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import psycopg2, psycopg2.extras
 from . import models
 from .database import engine
-from .routers import posts, users, auth
+from .routers import posts, users, auth, vote
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -10,7 +10,7 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)     
-
+app.include_router(vote.router)
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI root directory"}
---
+++ app/routers/vote.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException, FastAPI, Response, status, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import schemas,database, oauth2, models
+router = APIRouter(
+    prefix="/vote",
+    tags=['Vote']
+)
+
+@router.post("/", status_code= status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    vote_qurey = db.query(models.Votes.post_id).filter(models.Votes.post_id == vote.post_id, models.Votes.user_id == current_user.id)
+    found_vote = vote_qurey.first()
+    if(vote.dir == 1):
+        if found_vote:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail= f"User {current_user.id} has already voted on post {vote.post_id}")
+        new_vote = models.Votes(vote.post_id, current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "Succesfully voted"}
+    else:
+        if not found_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+        vote_qurey.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "Succesfully deleted vote"}
+    
